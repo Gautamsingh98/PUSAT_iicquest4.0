afd6281fe764c737fb50b583bece71bd86da6cb3
mrgautamsingh101 <mrgautamsingh101@gmail.com>
2026-06-11 11:31:08 +0545

    update

--- frontend/src/components/LeadTable.jsx
+++
@@ -1,9 +0,0 @@
-export default function LeadTable({ leads }) {
-  return (
-    <div className="bg-white rounded-xl shadow overflow-hidden">
-      <table className="min-w-full">
-        {/* table code here */}
-      </table>
-    </div>
-  );
-}
